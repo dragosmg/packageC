78f3a19dce4fd613f0297189ab40cb19dfd91288
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2023-11-15 19:52:51 +0000

    use `extra-repositories` for R setup and remove `zzz.R`

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  repos = getOption("repos")
-  repos["Internal CRAN"] = "https://dragosmg.github.io/internal-cran"
-  options(repos = repos)
-  invisible(repos)
-}
